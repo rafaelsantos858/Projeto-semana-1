552ed2d77fb72b9c9d2bce6c137a04c179af2318
rafaelsantos858 <69479684+rafaelsantos858@users.noreply.github.com>
2020-08-17 12:44:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+ $(document).ready(function () {
+    horario()
+
+ });
+
+
+function trocarimg(){
+    
+        document.getElementById("img1").src="https://wallpaperstock.net/wallpapers/thumbs1/17569wide.jpg"; 
+}
+function trocarimg2(){
+    
+    document.getElementById("img2").src="https://lh3.googleusercontent.com/proxy/J3ahGlp2KFIwACuQ747lKPaOfDr6R-Ibf38p8UWiFN5PWDrD0AmGfEpuxbPkDPf_gv1TwWR24DOHJDS6i3rZ7SN3tjo8Zjs3H3VWJ9nLf3yMoPZyWUycHnTo-6pWg1vaPoF5TuwgZFMyMSM"; 
+}
+
+
+function trocartitle(){
+    document.getElementById("title").innerHTML = 'O melhor dançarino de todos os tempos';
+    
+}
+
+
+    
